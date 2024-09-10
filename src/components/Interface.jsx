2e5a36175c055a62d069c1aa54918b54573d877d
Programmer-Shivansh <shivanshchauhan2005@gmail.com>
2024-09-10 16:40:06 +0530

    update

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -3,9 +3,10 @@ import {useCharacterAnimations} from "../context/CharAnimation";
 const Interfaces = ()=>{
     const {animations,animationsIndex,setAnimationsIndex} = useCharacterAnimations();
     return (
-        // <div>
-        //     <h1>Hello</h1>
-        // </div>
+        <>
+        <div style={{position: 'absolute', top: 0, right: 10, zIndex: 1000}}>
+            <h1>Bhoot in person</h1>
+            </div>
         <Affix style={{ position: 'absolute', top: 0, left: 10, zIndex: 1000 }}>
             
             <Stack>
@@ -21,6 +22,7 @@ const Interfaces = ()=>{
             }
             </Stack>
         </Affix>
+            </>
     )
 }
 
