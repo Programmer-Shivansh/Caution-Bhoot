4da5cd5999573b1e3b92bd0ac5b0b5f9c34c6f12
Programmer-Shivansh <shivanshchauhan2005@gmail.com>
2024-09-10 11:39:29 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { MantineProvider } from '@mantine/core'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    < MantineProvider withGlobalStyles withNormalizeCss>
+    < App />
+    </MantineProvider>
   </StrictMode>,
 )
